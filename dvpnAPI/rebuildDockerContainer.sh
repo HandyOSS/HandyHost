c6c09fbe609aa92d4af86c122a75433c2a953e89
Alex Smith <alexsmith_540@yahoo.com>
2022-04-01 21:26:37 -0600

    DVPN: ensure latest tag is used in rebuild operation

--- dvpnAPI/rebuildDockerContainer.sh
+++ dvpnAPI/rebuildDockerContainer.sh
@@ -5,6 +5,8 @@ USERHOME="$(eval echo ~$USERNAME)"
 #cp ./Dockerfile-hnsdfix ${USERHOME}/.HandyHost/sentinelData/dvpn-node/Dockerfile && \
 cd ${USERHOME}/.HandyHost/sentinelData/dvpn-node && \
 git stash && \
+git fetch --all && \
+git checkout "$1" && \
 docker build --file ${USERHOME}/.HandyHost/sentinelData/dvpn-node/Dockerfile \
   --tag sentinel-dvpn-node \
   --force-rm \
